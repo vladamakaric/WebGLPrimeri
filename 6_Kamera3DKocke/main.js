861e22307f4629890d95aefe69ab3d060102b8a1
Vladimir Makarić <vladamakaric@gmail.com>
2015-10-15 19:41:26 +0200

    camera pocetak

--- 6_Kamera3DKocke/main.js
+++ 6_Kamera3DKocke/main.js
@@ -7,108 +7,204 @@ var tColBuff;
 var sColBuff;
 var vPosition;
 var vertColor;
-var projectionTLoc, modelTLoc;
+var projectionTLoc, modelTLoc, cameraTLoc;
+
+function createCoordSys(gl){
+
+	//duzi koje definisu ose
+	var verts = [
+		//x
+		1.0,0.0,0.0, 
+		0.0,0.0,0.0,
+		//y
+		0.0,0.0,0.0,
+		0.0,1.0,0.0,
+		//z
+		0.0,0.0,0.0,
+		0.0,0.0,1.0
+	];
+
+	var colors = [
+		1.0, 0.0, 0.0, 1.0, 
+		1.0, 0.0, 0.0, 1.0, 
+
+		0.0, 1.0, 0.0, 1.0, 
+		0.0, 1.0, 0.0, 1.0, 
+
+		0.0, 0.0, 1.0, 1.0, 
+		0.0, 0.0, 1.0, 1.0
+	];
+
+	var vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	return {vertBuffer:vertexBuffer, colorBuffer:colorBuffer, vertSize:3, nVerts:6, colorSize:4, nColors: 6, primtype: gl.LINES};
+}
 
 
+function createCube(gl) {
+
+	// Vertex Data
+	var vertexBuffer;
+	vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+	var verts = [
+		// Front face
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+
+		// Back face
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+
+		// Top face
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+
+		// Bottom face
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		// Right face
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+
+		// Left face
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0
+			];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+	// Color data
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+	var faceColors = [
+		[1.0, 0.0, 0.0, 1.0], // Front face
+		[0.0, 1.0, 0.0, 1.0], // Back face
+		[0.0, 0.0, 1.0, 1.0], // Top face
+		[1.0, 1.0, 0.0, 1.0], // Bottom face
+		[1.0, 0.0, 1.0, 1.0], // Right face
+		[0.0, 1.0, 1.0, 1.0]  // Left face
+			];
+
+	var vertexColors = [];
+	for (var i in faceColors) {
+		var color = faceColors[i];
+		for (var j=0; j < 4; j++) {
+			vertexColors = vertexColors.concat(color);
+		}
+	}
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+	// Index data (defines the triangles to be drawn)
+	var cubeIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
+
+	var cubeIndices = [
+		0, 1, 2,      0, 2, 3,    // Front face
+		4, 5, 6,      4, 6, 7,    // Back face
+		8, 9, 10,     8, 10, 11,  // Top face
+		12, 13, 14,   12, 14, 15, // Bottom face
+		16, 17, 18,   16, 18, 19, // Right face
+		20, 21, 22,   20, 22, 23  // Left face
+			];
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
+
+	var cube = {vertBuffer:vertexBuffer, colorBuffer:colorBuffer, indxBuffer:cubeIndexBuffer,
+		vertSize:3, nVerts:24, colorSize:4, nColors: 24, nIndices:36,
+		primtype: gl.TRIANGLES};
+
+	return cube;
+}
+
+var cube;
+var coordSys;
 window.onload = function init()
 {
-    var canvas = document.getElementById( "gl-canvas" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+	var canvas = document.getElementById( "gl-canvas" );
 
-	var triangleVertices = new Float32Array([-1, -1,
-									  0, 1, 
-									  1, -1]);
+	gl = WebGLUtils.setupWebGL( canvas );
+	if ( !gl ) { alert( "WebGL isn't available" ); }
 
-    var squareVertices = new Float32Array([-1, -1, 
-									-1, 1, 
-									1, 1,
-									1, -1]);
+	gl.viewport( 0, 0, canvas.width, canvas.height );
 
-    var triangleColor = new Float32Array([
-        1.0, 0.0, 0.0, 1.0,
-        0.0, 1.0, 0.0, 1.0,
-        0.0, 0.0, 1.0, 1.0
-    ]);
+	gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+	gl.enable(gl.DEPTH_TEST);
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
 
-    var squareColor = new Float32Array([
-        1.0, 0.0, 0.0, 1.0,
-        1.0, 0.0, 0.0, 1.0,
-        1.0, 1.0, 0.0, 1.0,
-        1.0, 1.0, 0.0, 1.0
-    ]);
-
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-    
 	projectionTLoc = gl.getUniformLocation(program, "u_proj");
 	modelTLoc = gl.getUniformLocation(program, "u_model");
+	cameraTLoc = gl.getUniformLocation(program, "u_camera");
 
-	gl.uniformMatrix4fv(projectionTLoc, false, flatten(ortho(0, 100, 0, 100, -10,10)));
+	gl.uniformMatrix4fv(projectionTLoc, false, flatten(ortho(-100, 100, -100, 100, -200,200)));
 
-    vPosition = gl.getAttribLocation( program, "vPosition" );
-    vertColor = gl.getAttribLocation( program, "vertColor" );
+	vPosition = gl.getAttribLocation( program, "vPosition" );
+	vertColor = gl.getAttribLocation( program, "vertColor" );
 
-    gl.enableVertexAttribArray( vPosition );
-    gl.enableVertexAttribArray( vertColor );
+	gl.enableVertexAttribArray( vPosition );
+	gl.enableVertexAttribArray( vertColor );
 
-	///////////////////////////////////////
-    tPosBuff = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tPosBuff  );
-    gl.bufferData( gl.ARRAY_BUFFER, triangleVertices, gl.STATIC_DRAW );
-	/////////////////////////////////////////////
-    sPosBuff = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, sPosBuff );
-    gl.bufferData( gl.ARRAY_BUFFER, squareVertices, gl.STATIC_DRAW );
-	//////////////////////////////////////
-    sColBuff = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, sColBuff);
-    gl.bufferData( gl.ARRAY_BUFFER, squareColor, gl.STATIC_DRAW );
-	//////////////////////////////////////
-    tColBuff = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, tColBuff);
-    gl.bufferData( gl.ARRAY_BUFFER, triangleColor, gl.STATIC_DRAW );
-	//////////////////////////////////////
+	cube = createCube(gl);
+	coordSys = createCoordSys(gl);
 
 
-    render();
+
+	render();
 };
 
+var t=0;
 function render() {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+	gl.clear(gl.COLOR_BUFFER_BIT  | gl.DEPTH_BUFFER_BIT);
+	t+=0.02;
 
-	t+=0.1;
-	var primitiveArray = [0,1,1,0,1];
-	var positionArray = [[20,20], [40,40], [60,60], [10,100],[50,50]];
-	var primitiveLength = [3, 4];
-	var primitiveType = [gl.TRIANGLES, gl.TRIANGLE_FAN];
-	var primitiveColorBuffer = [tColBuff, sColBuff];
-	var primitivePosBuffer = [tPosBuff, sPosBuff];
-	var anim = true;
+	var modelMat;
+	gl.uniformMatrix4fv(cameraTLoc, false, flatten(lookAt([100*Math.cos(t),100,100*Math.sin(t)], [0,0,0], [0,1,0])));
 
-	primitiveArray.forEach(function(prim, indx){
-		gl.bindBuffer( gl.ARRAY_BUFFER, primitiveColorBuffer[prim]);
-		gl.vertexAttribPointer( vertColor, 4, gl.FLOAT, false, 0, 0 );
-		gl.bindBuffer( gl.ARRAY_BUFFER, primitivePosBuffer[prim]);
-		gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+	modelMat = scalem(10, 10, 10);
+	gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
+	//////////coordsys
+	gl.bindBuffer(gl.ARRAY_BUFFER, coordSys.vertBuffer);
+	gl.vertexAttribPointer(vPosition, coordSys.vertSize, gl.FLOAT, false, 0, 0);
 
-		var modelMat;
-		
-		if(prim==0)
-			modelMat = scalem(10, 10+5*Math.sin(t/2), 1);
-		else
-			modelMat = scalem(10+10*Math.sin(t), 10+10*Math.cos(t), 1);
+	gl.bindBuffer(gl.ARRAY_BUFFER, coordSys.colorBuffer);
+	gl.vertexAttribPointer(vertColor, coordSys.colorSize, gl.FLOAT, false, 0, 0);
 
+	gl.drawArrays(coordSys.primtype, 0, coordSys.nVerts);
 
-		modelMat = mult(translate(positionArray[indx][0], positionArray[indx][1], 0), modelMat);
+	//////////////////////////////////////////////////////////////////////////////cube
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube.vertBuffer);
+	gl.vertexAttribPointer(vPosition, cube.vertSize, gl.FLOAT, false, 0, 0);
 
-		gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
-		gl.drawArrays( primitiveType[prim],0, primitiveLength[prim] );
-	});
+	gl.bindBuffer(gl.ARRAY_BUFFER, cube.colorBuffer);
+	gl.vertexAttribPointer(vertColor, cube.colorSize, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.indxBuffer);
+
+	modelMat = mult(translate(-10, -10, -10), modelMat);
+
+	gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
+	gl.drawElements(cube.primtype, cube.nIndices, gl.UNSIGNED_SHORT, 0);
 
 	requestAnimFrame( render );
 }
