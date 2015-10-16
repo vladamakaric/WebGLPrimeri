21edb09f4d0b90f8fd5249705b1716dd45016c7d
Vladimir Makarić <vladamakaric@gmail.com>
2015-10-17 01:37:17 +0200

    poceo sa teksurama, dodao jednu, treba sa vise

---
+++ 9_Teksture/main.js
@@ -0,0 +1,191 @@
+var gl;
+var t = 0;
+var tPosBuff, sPosBuff;
+var tColBuff;
+var sColBuff;
+var vPosition;
+var vertColor;
+var projectionTLoc, modelTLoc, cameraTLoc;
+var texCoordLoc;
+
+var cube;
+var coordSys;
+var grid;
+var cameraY = 100;
+var cameraDistance = 100;
+
+var fovy = 60;
+var texture;
+function setUpEventHandling(canvas){
+
+	var angleInput = document.getElementById("angle");
+	angleInput.value = fovy;
+
+	angleInput.oninput = function(){
+		fovy = angleInput.value;
+	}
+
+	canvas.onmousewheel = function (event){
+		var wheel = event.wheelDelta/120;
+		cameraDistance+=wheel;
+		cameraDistance = Math.max(cameraDistance, 0);
+		console.log(cameraDistance);
+	}
+
+	document.onkeydown = checkKey;
+
+	function checkKey(e) {
+		e = e || window.event;
+
+		if (e.keyCode == '38') {
+			//up
+		   cameraY +=5; 
+		}
+		else if (e.keyCode == '40') {
+			//down
+		   cameraY -=5; 
+		}
+		else if (e.keyCode == '37') {
+		   // left arrow
+		}
+		else if (e.keyCode == '39') {
+		   // right arrow
+		}
+
+	}
+}
+
+window.onload = function init()
+{
+	var canvas = document.getElementById( "gl-canvas" );
+
+	gl = WebGLUtils.setupWebGL( canvas );
+	if ( !gl ) { alert( "WebGL isn't available" ); }
+
+	gl.viewport( 0, 0, canvas.width, canvas.height );
+
+	gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+	gl.enable(gl.DEPTH_TEST);
+	var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+	projectionTLoc = gl.getUniformLocation(program, "u_proj");
+	modelTLoc = gl.getUniformLocation(program, "u_model");
+	cameraTLoc = gl.getUniformLocation(program, "u_camera");
+
+	texCoordLoc = gl.getAttribLocation( program, "a_texcoord" );
+	vPosition = gl.getAttribLocation( program, "vPosition" );
+	vertColor = gl.getAttribLocation( program, "vertColor" );
+
+	gl.enableVertexAttribArray( texCoordLoc  );
+	gl.enableVertexAttribArray( vPosition );
+	// gl.enableVertexAttribArray( vertColor );
+
+	cube = createCube(gl);
+	coordSys = createCoordSys(gl);
+	grid = createGrid(40,40);
+
+	var texCoordBuff = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuff);
+	gl.enableVertexAttribArray(texCoordLoc);
+	gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+				0,1, 
+				1,1,
+				1,0,
+				0,0,
+
+				0,1, 
+				1,1,
+				1,0,
+				0,0,
+
+				0,1, 
+				1,1,
+				1,0,
+				0,0,
+
+				0,1, 
+				1,1,
+				1,0,
+				0,0,
+
+				0,1, 
+				1,1,
+				1,0,
+				0,0,
+
+				0,1, 
+				1,1,
+				1,0,
+				0,0
+					]), gl.STATIC_DRAW);
+
+
+	texture = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	// Fill the texture with a 1x1 blue pixel.
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE,
+				  new Uint8Array([255, 0, 0, 255]));
+	// Asynchronously load an image
+	var image = new Image();
+	image.src = "crate.jpg";
+	image.onload = function(){
+
+	  // document.write('<br><br><br>Your image in canvas: <img src="'+image.src+'" height="100" width="200"/>'); 
+	  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+	  gl.generateMipmap(gl.TEXTURE_2D);
+	}
+
+	setUpEventHandling(canvas);
+	render();
+
+};
+
+
+function render() {
+	gl.clear(gl.COLOR_BUFFER_BIT  | gl.DEPTH_BUFFER_BIT);
+	t+=0.009;
+
+	gl.uniformMatrix4fv(projectionTLoc, false, flatten(perspective(fovy, 1, 10, 1000)));
+
+	console.log(cameraDistance);
+	var modelMat;
+	gl.uniformMatrix4fv(cameraTLoc, false, flatten(lookAt([cameraDistance*Math.cos(t),(cameraDistance/100)*cameraY,cameraDistance*Math.sin(t)], [0,0,0], [0,1,0])));
+
+	modelMat = scalem(10, 10, 10);
+	gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
+
+	/////grid
+	
+	modelMat = scalem(20, 20, 20);
+	gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
+
+	// setAttributes(gl, grid, vPosition, vertColor);
+	// gl.lineWidth(1);
+	// gl.drawArrays(grid.primtype, 0, grid.nVerts);
+	// //////////coordsys
+	// setAttributes(gl, coordSys, vPosition, vertColor);
+    //
+	// gl.lineWidth(2);
+	// gl.drawArrays(coordSys.primtype, 0, coordSys.nVerts);
+	// ////cube
+	
+	setAttributes(gl, cube, vPosition, vertColor);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cube.indxBuffer);
+
+	var x= -100; 
+	var z= -100; 
+	
+	for(var i=0; i<5; i++){
+		for(var j=0; j<5; j++){
+			modelMat = mult(translate(x+i*30, 5, z+j*30), scalem(5,5,5));
+			gl.uniformMatrix4fv(modelTLoc, false, flatten(modelMat));
+			gl.drawElements(cube.primtype, cube.nIndices, gl.UNSIGNED_SHORT, 0);
+		}
+	}
+	
+
+	requestAnimFrame( render );
+}
---
+++ 9_Teksture/models.js
@@ -0,0 +1,178 @@
+function setAttributes(gl, obj, vAtribLoc, cAtribLoc){
+	gl.bindBuffer(gl.ARRAY_BUFFER, obj.vertBuffer);
+	gl.vertexAttribPointer(vAtribLoc, obj.vertSize, gl.FLOAT, false, 0, 0);
+
+	if(cAtribLoc>=0){
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, obj.colorBuffer);
+	gl.vertexAttribPointer(cAtribLoc, obj.colorSize, gl.FLOAT, false, 0, 0);
+	}
+}
+
+function createGrid(xLines, zLines){
+
+	var verts = [];
+	var colors = [];
+
+	var width = xLines-1;
+	var lenght = zLines-1;
+	
+	var x=-width/2;
+	var z=-lenght/2
+
+	for(var i=0; i<xLines; i++){
+			verts.push(x+i, 0, z);
+			verts.push(x+i, 0, -z);
+			colors.push(1,1,1,1);
+			colors.push(1,1,1,1);
+	}
+
+	for(var i=0; i<zLines; i++){
+			verts.push(x, 0, z+i);
+			verts.push(-x, 0, z+i);
+			colors.push(1,1,1,1);
+			colors.push(1,1,1,1);
+	}
+
+	console.log(verts);
+	console.log(colors);
+
+	var vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	return {vertBuffer:vertexBuffer, colorBuffer:colorBuffer, vertSize:3, nVerts:verts.length/3, colorSize:4, nColors: colors.length/4, primtype: gl.LINES};
+}
+
+function createCoordSys(gl){
+
+	//duzi koje definisu ose
+	var verts = [
+		//x
+		1.0,0.0,0.0, 
+		0.0,0.0,0.0,
+		//y
+		0.0,0.0,0.0,
+		0.0,1.0,0.0,
+		//z
+		0.0,0.0,0.0,
+		0.0,0.0,1.0
+	];
+
+	var colors = [
+		1.0, 0.0, 0.0, 1.0, 
+		1.0, 0.0, 0.0, 1.0, 
+
+		0.0, 1.0, 0.0, 1.0, 
+		0.0, 1.0, 0.0, 1.0, 
+
+		0.0, 0.0, 1.0, 1.0, 
+		0.0, 0.0, 1.0, 1.0
+	];
+
+	var vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+	return {vertBuffer:vertexBuffer, colorBuffer:colorBuffer, vertSize:3, nVerts:6, colorSize:4, nColors: 6, primtype: gl.LINES};
+}
+
+
+function createCube(gl) {
+
+	// Vertex Data
+	var vertexBuffer;
+	vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+	var verts = [
+		// Front face
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+
+		// Back face
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+
+		// Top face
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+
+		// Bottom face
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+
+		// Right face
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+
+		// Left face
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0
+			];
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+
+	// Color data
+	var colorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+
+	var faceColors = [
+		[1.0, 0.0, 0.0, 1.0], // Front face
+		[0.0, 1.0, 0.0, 1.0], // Back face
+		[0.0, 0.0, 1.0, 1.0], // Top face
+		[1.0, 1.0, 0.0, 1.0], // Bottom face
+		[1.0, 0.0, 1.0, 1.0], // Right face
+		[0.0, 1.0, 1.0, 1.0]  // Left face
+			];
+
+	var vertexColors = [];
+	for (var i in faceColors) {
+		var color = faceColors[i];
+		for (var j=0; j < 4; j++) {
+			vertexColors = vertexColors.concat(color);
+		}
+	}
+
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexColors), gl.STATIC_DRAW);
+
+	// Index data (defines the triangles to be drawn)
+	var cubeIndexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeIndexBuffer);
+
+	var cubeIndices = [
+		0, 1, 2,      0, 2, 3,    // Front face
+		4, 5, 6,      4, 6, 7,    // Back face
+		8, 9, 10,     8, 10, 11,  // Top face
+		12, 13, 14,   12, 14, 15, // Bottom face
+		16, 17, 18,   16, 18, 19, // Right face
+		20, 21, 22,   20, 22, 23  // Left face
+			];
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeIndices), gl.STATIC_DRAW);
+
+	var cube = {vertBuffer:vertexBuffer, colorBuffer:colorBuffer, indxBuffer:cubeIndexBuffer,
+		vertSize:3, nVerts:24, colorSize:4, nColors: 24, nIndices:36,
+		primtype: gl.TRIANGLES};
+
+	return cube;
+}
